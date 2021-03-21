25cae483eca0637a255cfc579c5f082fdc5b1c9c
高橋京司 <takahashikeiji@takahashikyoujinoMacBook-Air.local>
2021-03-21 17:10:00 +0900

    modfied

--- model/model.py
+++ model/model.py
@@ -6,9 +6,9 @@ import timm
 class FlowerImgClassifier(nn.Module):
     def __init__(self, model_arch, n_class, pretrained=True):
         super().__init__()
-        self.model = timm.create_model(model_arch, pretrained=pretrained)
-        n_features = self.model.classifier.in_features
-        self.model.classifier = nn.Linear(n_features, n_class)
+        self.model = timm.create_model(model_arch, pretrained=pretrained, num_classes=n_class)
+        # n_features = self.model.classifier.in_features
+        # self.model.classifier = nn.Linear(n_features, n_class)
 
     def forward(self, x):
         x = self.model(x)
