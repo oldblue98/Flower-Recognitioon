268b1880e390e6760df6d700bab20c81e1bdc340
高橋京司 <takahashikeiji@takahashikyoujinoMacBook-Air.local>
2021-03-21 14:54:35 +0900

    modfied

--- model/model.py
+++ model/model.py
@@ -8,7 +8,7 @@ class FlowerImgClassifier(nn.Module):
         super().__init__()
         self.model = timm.create_model(model_arch, pretrained=pretrained)
         n_features = self.model.classifier.in_features
-        self.model.classifier = nn.Linear(n_features, n_class)
+        self.model.head = nn.Linear(n_features, n_class)
 
     def forward(self, x):
         x = self.model(x)
