15162657f270a9e18df9ddeeb5628a0e3ff7b0e9
高橋京司 <takahashikeiji@takahashikyoujinoMacBook-Air.local>
2021-03-21 12:40:19 +0900

    modfied

--- infer.py
+++ infer.py
@@ -41,6 +41,7 @@ from model.utils import seed_everything
 
 
 test = pd.DataFrame()
+base_test_data_path = './data/test/'
 test['image_path'] = [os.path.join(base_test_data_path, f) for f in os.listdir('./data/test/')]
 test = test.sort_values('image_path').reset_index(drop=True)
 
