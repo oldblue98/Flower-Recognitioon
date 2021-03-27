48455fd3a2aec9ca99e89a2990580f03de2fdf93
高橋京司 <takahashikeiji@takahashikyoujinoMacBook-Air.local>
2021-03-27 15:29:06 +0900

    modified

--- train_all.py
+++ train_all.py
@@ -23,13 +23,13 @@ CFG_list = [
     # "./configs/tf_efficientnet_b2_ver2.json",
     # "./configs/tf_efficientnet_b3_ver2.json",
     # "./configs/tf_efficientnet_b4_ver2.json",
-    # "./configs/inception_resnet_v2.json",
-    # "./configs/seresnext50_32x4d.json",
-    "./configs/skresnext50_32x4d.json",
+    "./configs/inception_resnet_v2.json",
+    "./configs/seresnext50_32x4d.json",
+    "./configs/vit_base_patch16_224_ver2.json",
+    "./configs/vit_base_resnet50d_224_ver2.json",
     "./configs/tf_efficientnet_b2_ns.json",
     "./configs/tf_efficientnet_b3_ns.json",
-    "./configs/vit_base_patch16_224_ver2.json",
-    "./configs/vit_base_resnet50d_224_ver2.json"
+    "./configs/skresnext50_32x4d.json"
 ]
 # logger の設定
 from logging import getLogger, StreamHandler,FileHandler, Formatter, DEBUG, INFO
